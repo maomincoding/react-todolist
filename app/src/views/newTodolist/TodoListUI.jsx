9c94b073369b7d451814196589a928112c4d71ff
maomincoding <17864296568@163.com>
2020-04-30 17:57:59 +0800

    2020.4.30 17:57

--- app/src/views/newTodolist/TodoListUI.jsx
+++ app/src/views/newTodolist/TodoListUI.jsx
@@ -1,23 +1,44 @@
 import React from 'react'
 import { Button, Input, List, Divider } from 'antd'
 
-export default class TodolistUI extends React.Component {
-  render() {
-    return (
-      <div style={{ margin: '20px' }}>
-        <Input value={this.props.inputValue} placeholder="Basic usage" style={{ width: '300px' }} onChange={this.props.handerChange} />
-        <Button type="primary" style={{ marginLeft: '20px' }} onClick={this.props.handerClick}>提交</Button>
-        <Divider orientation="left">列表</Divider>
-        <List
-          bordered
-          dataSource={this.props.list}
-          renderItem={(item, index) => (
-            <List.Item onClick={()=>{this.props.delete(index)}} >
-              {item}
-            </List.Item>
-          )}
-        />
-      </div>
-    )
-  }
+// 无状态组件,性能高。就是一个函数，一般用在UI组件。
+const TodolistUI = (props)=>{
+  return (
+    <div style={{ margin: '20px' }}>
+    <Input value={props.inputValue} placeholder="Basic usage" style={{ width: '300px' }} onChange={props.handerChange} />
+    <Button type="primary" style={{ marginLeft: '20px' }} onClick={props.handerClick}>提交</Button>
+    <Divider orientation="left">列表</Divider>
+    <List
+      bordered
+      dataSource={props.list}
+      renderItem={(item, index) => (
+        <List.Item onClick={()=>{props.delete(index)}} >
+          {item}
+        </List.Item>
+      )}
+    />
+  </div>
+  )
 }
+export default TodolistUI
+
+// export default class TodolistUI extends React.Component {
+//   render() {
+//     return (
+//       <div style={{ margin: '20px' }}>
+//         <Input value={this.props.inputValue} placeholder="Basic usage" style={{ width: '300px' }} onChange={this.props.handerChange} />
+//         <Button type="primary" style={{ marginLeft: '20px' }} onClick={this.props.handerClick}>提交</Button>
+//         <Divider orientation="left">列表</Divider>
+//         <List
+//           bordered
+//           dataSource={this.props.list}
+//           renderItem={(item, index) => (
+//             <List.Item onClick={()=>{this.props.delete(index)}} >
+//               {item}
+//             </List.Item>
+//           )}
+//         />
+//       </div>
+//     )
+//   }
+// }
