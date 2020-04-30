077629d3aa68494658003cff8e1132dbfd22d9fd
maomincoding <17864296568@163.com>
2020-04-30 14:40:32 +0800

    2020.4.30 14:40

--- app/src/store/reducer.js
+++ app/src/store/reducer.js
@@ -1,14 +1,30 @@
-// Reducer  存数据以及处理数据。 reducer可以接受state，但不能修改state
+import {CHANGE_INPUT_VALUE,CHANGE_ITEM_VALUE,DELETE_ITEM_VALUE} from './actionType' 
+
+// Reducer  存数据以及处理数据。 reducer可以接受state，但不能修改state。
 const defaultState = {
   inputValue:"",
-  list:['1','2']
+  list:[]
 }
 
 // 首先必须返回函数
 export default (state=defaultState,action) => {
-  if(action.type === 'change_input_value'){
-    const newState = JSON.parse(JSON.stringify(state));
+  // input框内值
+  if(action.type === CHANGE_INPUT_VALUE){
+    const newState = JSON.parse(JSON.stringify(state)); // 深拷贝
     newState.inputValue = action.value;
+    return newState
+  }
+  // 列表项
+  if(action.type === CHANGE_ITEM_VALUE){
+    const newState = JSON.parse(JSON.stringify(state)); // 深拷贝
+    newState.list.push(newState.inputValue);
+    newState.inputValue = ""
+    return newState
+  }
+  // 删除
+  if(action.type === DELETE_ITEM_VALUE){
+    const newState = JSON.parse(JSON.stringify(state)); // 深拷贝
+    newState.list.splice(action.i,1);
     return newState
   }
   return state
--- app/src/views/newTodolist/TodoList.jsx
+++ app/src/views/newTodolist/TodoList.jsx
@@ -1,26 +1,30 @@
 import React, { Component } from 'react'
 import { Button, Input, List , Divider } from 'antd';
 import store from '../../store/index'
+import {CHANGE_INPUT_VALUE,CHANGE_ITEM_VALUE,DELETE_ITEM_VALUE} from '../../store/actionType'
 
 export default class TodoList extends Component {
   constructor(props){
     super(props);
     this.state = store.getState(); // 获取store
+
     this.handerChange = this.handerChange.bind(this);
     this.handerStateChange = this.handerStateChange.bind(this);
+    this.handerClick = this.handerClick.bind(this);
+
     store.subscribe(this.handerStateChange) //  订阅 store store 只要发生改变。方法内的函数自动执行。
   }
   render() {
     return (
       <div style={{ margin: '20px' }}>
         <Input value={this.state.inputValue}  placeholder="Basic usage" style={{ width: '300px' }}  onChange={this.handerChange}/>
-        <Button type="primary" style={{ marginLeft: '20px' }}>提交</Button>
+        <Button type="primary" style={{ marginLeft: '20px' }} onClick={this.handerClick}>提交</Button>
         <Divider orientation="left">列表</Divider>
         <List
           bordered
           dataSource={this.state.list}
-          renderItem={item => (
-            <List.Item>
+          renderItem={(item,index) => (
+            <List.Item onClick={this.delete.bind(this,index)}>
               {item}
             </List.Item>
           )}
@@ -28,17 +32,35 @@ export default class TodoList extends Component {
       </div>
     )
   }
+  // 组件更新状态
+    handerStateChange(){
+      this.setState(store.getState()); // 重新获取store，组件更新状态，替换原来的数据
+  }
+  // input值检测
   handerChange(e){
     // 创建action
     const action = {
-      type:'change_input_value', // 描述
+      type: CHANGE_INPUT_VALUE, // 描述
       value: e.target.value
     }
     store.dispatch(action); // 将action 传给store 
   }
-  handerStateChange(){
-    this.setState(store.getState()); // 重新获取store，组件更新状态，替换原来的数据
+  // 提交
+  handerClick(){
+    const action = {
+      type: CHANGE_ITEM_VALUE,
+    }
+    store.dispatch(action);
   }
+  // 删除
+  delete(i){
+    const action = {
+      type: DELETE_ITEM_VALUE,
+      i
+    }
+    store.dispatch(action);
+  }
+  // 组件挂载之后
   componentDidMount() {
     document.title = "使用Ant 搭建TodoList"
   }
