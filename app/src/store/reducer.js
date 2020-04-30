38907e072f549e983cd03b3643edd629f6a0cc9e
maomincoding <17864296568@163.com>
2020-04-30 12:08:28 +0800

    2020.4.30 12:08

--- app/src/store/reducer.js
+++ app/src/store/reducer.js
@@ -1,8 +1,17 @@
+// Reducer  存数据以及处理数据。 reducer可以接受state，但不能修改state
 const defaultState = {
   inputValue:"",
-  list:[]
+  list:['1','2']
 }
 
+// 首先必须返回函数
 export default (state=defaultState,action) => {
+  if(action.type === 'change_input_value'){
+    const newState = JSON.parse(JSON.stringify(state));
+    newState.inputValue = action.value;
+    return newState
+  }
   return state
 }
+
+// state参数表示store里的上一次数据 ， actions 是组件传过来需要修改的值
--- app/src/views/newTodolist/TodoList.jsx
+++ app/src/views/newTodolist/TodoList.jsx
@@ -1,25 +1,24 @@
 import React, { Component } from 'react'
 import { Button, Input, List , Divider } from 'antd';
-
-const data = [
-  'Racing car sprays burning fuel into crowd.',
-  'Japanese princess to wed commoner.',
-  'Australian walks 100km after outback crash.',
-  'Man charged over missing wedding girl.',
-  'Los Angeles battles huge wildfires.',
-];
-
+import store from '../../store/index'
 
 export default class TodoList extends Component {
+  constructor(props){
+    super(props);
+    this.state = store.getState(); // 获取store
+    this.handerChange = this.handerChange.bind(this);
+    this.handerStateChange = this.handerStateChange.bind(this);
+    store.subscribe(this.handerStateChange) //  订阅 store store 只要发生改变。方法内的函数自动执行。
+  }
   render() {
     return (
       <div style={{ margin: '20px' }}>
-        <Input placeholder="Basic usage" style={{ width: '300px' }} />
+        <Input value={this.state.inputValue}  placeholder="Basic usage" style={{ width: '300px' }}  onChange={this.handerChange}/>
         <Button type="primary" style={{ marginLeft: '20px' }}>提交</Button>
         <Divider orientation="left">列表</Divider>
         <List
           bordered
-          dataSource={data}
+          dataSource={this.state.list}
           renderItem={item => (
             <List.Item>
               {item}
@@ -28,5 +27,19 @@ export default class TodoList extends Component {
         />
       </div>
     )
+  }
+  handerChange(e){
+    // 创建action
+    const action = {
+      type:'change_input_value', // 描述
+      value: e.target.value
+    }
+    store.dispatch(action); // 将action 传给store 
+  }
+  handerStateChange(){
+    this.setState(store.getState()); // 重新获取store，组件更新状态，替换原来的数据
+  }
+  componentDidMount() {
+    document.title = "使用Ant 搭建TodoList"
   }
 }
